a5a98e55d209eb41d19d717c095375be8f9060d3
Aleksandr <bestprogreraxe@gmail.com>
2022-12-25 07:12:59 +0300

    solved Task47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -5,3 +5,60 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9  */
 
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Error: wrong entry.");
+        }
+    }
+    return result;
+}
+
+double[,] InitRandomMatrix(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * 100, 3);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintArray(string message, double[,] matrix)
+{
+    Console.WriteLine(message);
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j] } ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Enter the number of rows:");
+int n = GetNumber("Enter the number of columns:");
+
+double[,] matrix = InitRandomMatrix(m, n);
+
+PrintArray($"Your random matrix of random real numbers with {m} rows and {n} columns:", matrix);
